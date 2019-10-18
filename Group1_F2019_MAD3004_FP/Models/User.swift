39607329e7c7176232da6cc9c931532d5ad19a02
Karan Kumar <Kumarkay.ca@gmail.com>
2019-10-18 00:33:47 -0400

    json file

---
+++ Group1_F2019_MAD3004_FP/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  Data.swift
+//  Group1_F2019_MAD3004_FP
+//
+//  Created by Sandeep Jangra on 2019-10-17.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+struct User {
+    var id: Int?
+    var name: String?
+    var age: Int?
+}
