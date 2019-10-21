4601094eb5dc2bd901a8cf6dd5b67134143c21c2
Karan Kumar <Kumarkay.ca@gmail.com>
2019-10-21 01:50:52 -0400

    Calculating Year of Birth Using get

--- Group1_F2019_MAD3004_FP/Employee/Employee.swift
+++ Group1_F2019_MAD3004_FP/Employee/Employee.swift
@@ -11,6 +11,16 @@ class Employee{
     var name: String
     var age: Int
     var earning: Float = 1000.0
+    var yearofBirth : Int
+    {
+        get {
+                    let date = Date()
+                    let calendar = Calendar.current
+                    let components = calendar.dateComponents([.year], from: date)
+                    let year = components.year!
+                    return year-age
+        }
+    }
     
     init(EmpName:String,EmpAge:Int,EmpEarning:Float)
     {
@@ -23,15 +33,15 @@ class Employee{
     
     // created By Anikta and Karan
     //https://www.zerotoappstore.com/get-year-month-day-from-date-swift.html
-    func calBirthYear() -> Int {
-        let date = Date()
-        let calendar = Calendar.current
-        let components = calendar.dateComponents([.year], from: date)
-        let year = components.year!
-        return year-age
-        
-        
-    }
+//    func calBirthYear() -> Int {
+//        let date = Date()
+//        let calendar = Calendar.current
+//        let components = calendar.dateComponents([.year], from: date)
+//        let year = components.year!
+//        return year-age
+//
+//
+//    }
     
 //   func calEarnings() -> <#return type#>
 //   {
