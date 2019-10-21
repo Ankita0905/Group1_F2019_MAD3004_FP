d778417b4b010b31947a0180165333e9ca211e10
Karan Kumar <Kumarkay.ca@gmail.com>
2019-10-21 10:05:40 -0400

    Adding EmpType to Intern

--- Group1_F2019_MAD3004_FP/Employee/Intern.swift
+++ Group1_F2019_MAD3004_FP/Employee/Intern.swift
@@ -14,14 +14,16 @@ class Intern:Employee,IPrintable
    
     var schoolName:String
     
-    init(IName:String,IAge:Int,IEarning:Float,SchoolName:String)
+    init(IName:String,IAge:Int,IEarning:Float,SchoolName:String,IEmpType: String)
     {
         self.schoolName=SchoolName
-        super.init(EmpName: IName, EmpAge: IAge, EmpEarning: IEarning)
+        super.init(EmpName: IName, EmpAge: IAge, EmpEarning: IEarning, EmpType: IEmpType)
+        //super.init(Emptype : IEmpType,EmpName: IName, EmpAge: IAge, EmpEarning: IEarning)
     }
     
-    func printMyData() {
-        print(" Employee Is Intern")
+    func printMyData()
+    {
+        print("Employee Is \(empType)")
         print(" Name :\(name)\n","Year Of Birth :\(yearofBirth)\n","School :\(schoolName)")
     }
    
