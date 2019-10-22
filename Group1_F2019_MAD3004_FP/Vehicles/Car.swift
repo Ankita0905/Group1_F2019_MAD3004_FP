f10c40f5b620a37586423a6093257614d2db1594
Sandeepsingh2810 <Sandeepjangra0100@gmail.com>
2019-10-22 18:36:25 -0400

    error in vehicle

--- Group1_F2019_MAD3004_FP/Vehicles/Car.swift
+++ Group1_F2019_MAD3004_FP/Vehicles/Car.swift
@@ -9,9 +9,10 @@
 import Foundation
 enum CarError: Error
 {
+    case invalidNoOfDoors(noOfDoor: Int)
 }
 
-enum cartype
+enum CarType
 {
     case manual
     case Automatic
@@ -24,12 +25,41 @@ class Car: Vehicle
   
     var noOfDoor: Int
     var Convertable: Bool
-    init(noOfDoor: Int, Convertable: Bool, eId: Int, make : String, model : String, plateNum: String, CommBY: commuteBy)
+    var carType: CarType
+    init(noOfDoor: Int, Convertable: Bool, carType: CarType, eId: Int, make : String, model : String, plateNum: String, CommBY: commuteBy) throws
     {
-     self.noOfDoor=noOfDoor
- self.Convertable=Convertable
-        super.init(eId: eId , make: make, model: model , plateNum: plateNum, CommBY: CommBY)
-    }
+        if noOfDoor > 4
+        {
+            throw CarError.invalidNoOfDoors(noOfDoor: noOfDoor)
+        }
+           self.noOfDoor=noOfDoor
+           self.Convertable=Convertable
+           self.carType=carType
+           super.init(eId: eId , make: make, model: model , plateNum: plateNum, CommBY: CommBY)
     
-
+    
+    
+  /*  catch CarError.invalidNoOfDoors(let noOfDoor)
+    {
+    print("Inavlid Update Name : \(noOfDoor)")
+    }*/
+        func SetFacultyNam(name: Int) throws {
+            do{
+                if noOfDoor > 4
+                {
+                    throw CarError.invalidNoOfDoors(noOfDoor: noOfDoor)
+                }
+               self.noOfDoor=noOfDoor
+                
+            }
+            catch CarError.invalidNoOfDoors(let facultyName)
+            {
+                print("Inavlid Update Name : \(facultyName)")
+            }
+            
+            
+            
+            
+        }
+}
 }
